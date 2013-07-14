7baa83709b2e1bbfd545c963a6657786e8053752
TomOne <user.email@fake.com>
2013-07-14 16:59:40 +0200

    bumped opera to v15.0

--- Opera/Tools/ChocolateyInstall.ps1
+++ Opera/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,20 @@
-﻿Install-ChocolateyPackage 'opera' 'exe' '/silent /launchopera 0 /setdefaultbrowser 0' 'http://get.geo.opera.com/pub/opera/win/1215/int/Opera_1215_int_Setup.exe'
+﻿$packageName = 'opera'
+$fileType = 'exe'
+$silentArgs = '/install /silent /launchopera 0 /quicklaunchshortcut 0 /setdefaultbrowser 0'
+$url = 'http://get.geo.opera.com/pub/opera/desktop/15.0.1147.141/win/Opera_15.0.1147.141_Setup.exe'
+
+$tempDir = "$env:TEMP\chocolatey\$packageName"
+if (!(Test-Path $tempDir)) {New-Item $tempDir -ItemType directory -Force}
+$fileFullPath = "$tempDir\${packageName}Install.exe"
+
+Get-ChocolateyWebFile $packageName $fileFullPath $url
+
+$extractDir = "$tempDir\${packageName}Install"
+
+Start-Process '7za' -ArgumentList "x -o`"$extractDir`" -y `"$fileFullPath`"" -Wait
+
+$file = "$extractDir\launcher.exe"
+
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file
+
+Remove-Item $extractDir -Force -Recurse
